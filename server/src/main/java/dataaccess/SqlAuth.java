8bae118c2dc1162027a04c4efaa8b95fac93f2f3
Tayler Hilton <Taylerch@byu.edu>
2025-03-11 22:24:20 -0600

    SqlAuth first draft

--- server/src/main/java/dataaccess/SqlAuth.java
+++ server/src/main/java/dataaccess/SqlAuth.java
@@ -13,25 +13,36 @@ public class SqlAuth implements AuthDAO{
     }
 
     public AuthData createAuth(AuthData auth) throws ResponseException{
-//        var statement = "INSERT INTO pet (name, type, json) VALUES (?, ?, ?)";
-//        var json = new Gson().toJson(pet);
-//        var id = executeUpdate(statement, pet.name(), pet.type(), json);
-//        return new Pet(id, pet.name(), pet.type());
-        return null;
+        var statement = "INSERT INTO auth (authToken, username) VALUES (?, ?)";
+        executeUpdate(statement, auth.authToken(), auth.username());
+        return auth;
     }
 
     public AuthData getAuth(String authToken) throws ResponseException{
+        try (var conn = DatabaseManager.getConnection()) {
+            var statement = "SELECT authToken, username FROM auth WHERE authToken=?";
+            try (var ps = conn.prepareStatement(statement)) {
+                ps.setString(1, authToken);  // not sure if this is correct, don't really understand what's going on here.
+                try (var rs = ps.executeQuery()) {
+                    if (rs.next()) {
+                        return readAuth(rs);
+                    }
+                }
+            }
+        } catch (Exception e) {
+            throw new ResponseException(500, String.format("Unable to read data: %s", e.getMessage()));
+        }
         return null;
     }
 
     public void deleteAuth(String authToken) throws ResponseException{
-//        var statement = "DELETE FROM pet WHERE id=?";
-//        executeUpdate(statement, id);
+        var statement = "DELETE FROM auth WHERE authToken=?";
+        executeUpdate(statement, authToken);
     }
 
     public void clearAuths() throws ResponseException{
-//        var statement = "TRUNCATE pet";
-//        executeUpdate(statement);
+        var statement = "TRUNCATE auth";
+        executeUpdate(statement);
     }
 
     private final String[] createStatements = {
@@ -42,6 +53,34 @@ public class SqlAuth implements AuthDAO{
             ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
             """
     };
+
+    private AuthData readAuth(ResultSet rs) throws SQLException {
+        var authToken = rs.getString("authToken");
+        var username = rs.getString("username");
+        return new AuthData(authToken, username);
+    }
+
+    private int executeUpdate(String statement, Object... params) throws ResponseException {
+        try (var conn = DatabaseManager.getConnection()) {
+            try (var ps = conn.prepareStatement(statement, RETURN_GENERATED_KEYS)) {
+                for (var i = 0; i < params.length; i++) {
+                    var param = params[i];
+                    if (param instanceof String p) ps.setString(i + 1, p);
+                    else if (param == null) ps.setNull(i + 1, NULL);
+                }
+                ps.executeUpdate();
+
+                var rs = ps.getGeneratedKeys();
+                if (rs.next()) {
+                    return rs.getInt(1);
+                }
+
+                return 0;
+            }
+        } catch (SQLException e) {
+            throw new ResponseException(500, String.format("unable to update database: %s, %s", statement, e.getMessage()));
+        }
+    }
 
 
     private void configureAuthDatabase() throws ResponseException {
