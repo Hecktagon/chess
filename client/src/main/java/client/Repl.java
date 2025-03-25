e3adc90db9c187a0514179b6cb2bc59384bacc10
Hecktagon <tayler.c.hilton@gmail.com>
2025-03-24 20:59:50 -0600

    Edits made to repl

--- client/src/main/java/client/Repl.java
+++ client/src/main/java/client/Repl.java
@@ -11,7 +11,7 @@ public class Repl {
     }
 
     public void run() {
-        System.out.println("\uD83D\uDC36 Welcome to the pet store. Sign in to start.");
+        System.out.println(SET_TEXT_BOLD + SET_TEXT_COLOR_BLUE + "Welcome to Chess! type 'help' for options." + RESET_TEXT_COLOR + RESET_TEXT_BOLD_FAINT);
         System.out.print(client.help());
 
         Scanner scanner = new Scanner(System.in);
@@ -34,53 +34,3 @@ public class Repl {
         System.out.print("\n" + RESET_TEXT_COLOR + ">>> " + SET_TEXT_COLOR_BLUE);
     }
 }
-
-// petshop code:
-
-//package client;
-//
-//import client.websocket.NotificationHandler;
-//import webSocketMessages.Notification;
-//
-//import java.util.Scanner;
-//
-//import static client.EscapeSequences.*;
-//
-//public class Repl implements NotificationHandler {
-//    private final PetClient client;
-//
-//    public Repl(String serverUrl) {
-//        client = new PetClient(serverUrl, this);
-//    }
-//
-//    public void run() {
-//        System.out.println("\uD83D\uDC36 Welcome to the pet store. Sign in to start.");
-//        System.out.print(client.help());
-//
-//        Scanner scanner = new Scanner(System.in);
-//        var result = "";
-//        while (!result.equals("quit")) {
-//            printPrompt();
-//            String line = scanner.nextLine();
-//
-//            try {
-//                result = client.eval(line);
-//                System.out.print(BLUE + result);
-//            } catch (Throwable e) {
-//                var msg = e.toString();
-//                System.out.print(msg);
-//            }
-//        }
-//        System.out.println();
-//    }
-//
-//    public void notify(Notification notification) {
-//        System.out.println(RED + notification.message());
-//        printPrompt();
-//    }
-//
-//    private void printPrompt() {
-//        System.out.print("\n" + RESET + ">>> " + GREEN);
-//    }
-//
-//}
