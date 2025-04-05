d39c6a32f8fae06613aad8f9535881035c568f11
Hecktagon <tayler.c.hilton@gmail.com>
2025-04-05 16:54:03 -0600

    Added a framework for server websocket

---
+++ server/src/main/java/websocket/WebSocketHandler.java
@@ -0,0 +1,21 @@
+package websocket;
+
+public class WebSocketHandler {
+    //    WebSocketSessions sessions;
+
+    //    @onWebSocketError
+    //    void onError(Throwable throwable);
+
+    //    @onWebSocketMessage
+    //    void onMessage(session, string)
+        //    1. Determine message type
+        //    2. call one of the below methods to process the message
+
+    //    func connect(message)
+    //    func makeMove(message)
+    //    func leaveGame(message)
+    //    func resignGame(message)
+
+    //    func sendMessage(message, session)
+    //    func broadcastMessage(gameID, message, exceptThisSession);
+}
---
+++ server/src/main/java/websocket/WebSocketSessions.java
@@ -0,0 +1,12 @@
+package websocket;
+
+import java.util.HashMap;
+import java.util.HashSet;
+
+public class WebSocketSessions {
+    //    HashMap<GameID, HashSet<Session>> sessionMap;
+
+    //    void addSessionToGame(gameID, session);
+    //    void removeSessionFromGame(gameID, session);
+    //    HashSet<Session> getSessionsForGame(gameID);
+}
