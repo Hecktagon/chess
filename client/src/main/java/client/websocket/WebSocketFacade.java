7eb46996cfed28c84f22e9282171faedf46ee63a
Hecktagon <tayler.c.hilton@gmail.com>
2025-04-05 17:32:12 -0600

    Started a framework for client websocket

---
+++ client/src/main/java/client/websocket/WebSocketFacade.java
@@ -0,0 +1,32 @@
+package client.websocket;
+
+// extends endpoint and implements some interface
+public class WebSocketFacade{
+    //    Session session;
+    //    GameHandler gameHandler;
+
+    //    @Override
+    //    onOpen(...); overridden from endpoint
+    //
+    //    @Override
+    //    onClose(...); overridden from endpoint
+    //
+    //    @Override
+    //    onError(...); overridden from endpoint
+
+    // OUTGOING MESSAGES
+    //    connect(...)
+    //    makeMove(...)
+    //    leaveGame(...)
+    //    resignGame(...)
+
+    //    private
+    //    sendMessage(...)
+    //        1. create command message
+    //        2. send message to server
+
+    //    process incoming messages:
+    //    onMessage(message)
+    //        1. deserialize message
+    //        2. call GameHandler to process message
+}
